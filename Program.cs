220bf8ac8082eb8958eb15c87092167a3984d644
Ganesh Kadam <ganesh89kadam@gmail.com>
2020-09-05 08:10:07 +0530

    internal commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace StackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<object> stk = new Stack<object>();
+
+            for (int i=0;i<4;i++)
+            {
+                
+                 Console.WriteLine("insert to push:");
+                object x = Console.ReadLine();
+
+
+                if (x == null)
+                {
+                    Console.WriteLine("null is not allowed");
+
+                }
+                else {
+                    
+                    stk.Push(x);
+
+                }
+
+             }
+           
+       
+
+                object p=stk.Pop();
+                Console.WriteLine("item is removed:{0}",p);
+
+            foreach (object i in stk)
+            {
+                Console.WriteLine(i);
+
+            }
+
+            stk.Clear();
+          
+        }
+    }
+
+}
